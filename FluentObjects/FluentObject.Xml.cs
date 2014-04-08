e6810b51e346a98969b75ff518bbfd74f485b5c0
Graeme Wicksted <graeme.wicksted@gmail.com>
2014-04-07 21:07:34 -0400

    Added XML support
    Much of XML is in place.  Need to test how close it can be serialized
    then deserialized back to the original structure.

---
+++ FluentObjects/FluentObject.Xml.cs
@@ -0,0 +1,157 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace FluentObjects
+{
+    public partial class FluentObject : IXmlSerializable 
+    {
+        public XmlSchema GetSchema()
+        {
+            return null; // sufficient
+        }
+
+        // TODO: handle arrays
+        public string ToXmlString(string name)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            // TODO: specify encoding as UTF-8
+
+            using (TextWriter textWriter = new StringWriter(stringBuilder))
+            {
+                using (TextWriter indentedTextWriter = new IndentedTextWriter(textWriter, "  "))
+                {
+                    using (XmlWriter writer = new XmlTextWriter(indentedTextWriter))
+                    {
+                        writer.WriteStartDocument();
+
+                        WriteXmlElement(writer, this, name);
+
+                        writer.WriteEndDocument();
+                        writer.Flush();
+                    }
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        // TODO: handle arrays
+        public void WriteXml(XmlWriter writer)
+        {
+            WriteXmlElement(writer, this, null);
+        }
+
+        private void WriteXmlElement(XmlWriter writer, FluentObject elementFo, string name)
+        {
+            if (name != null)
+            {
+                writer.WriteStartElement(name);
+            }
+
+            // TODO: should test to see if is native datatype (struct) vs object -- if object, it is element
+            var xmlAttributes = (from a in elementFo._attributes where !(a.Value is FluentObject) select new KeyValuePair<string, string>(a.Key, a.Value.ToString()));
+            WriteXmlAttributes(writer, xmlAttributes);
+            // TODO: selectmany
+            // TODO: dictionary keys may not be valid xml attribute names
+            xmlAttributes = (from a in elementFo._dictionaryElements where !(a.Value is FluentObject) select new KeyValuePair<string, string>(a.Key.ToString(), a.Value.ToString()));
+            WriteXmlAttributes(writer, xmlAttributes);
+
+            if (name != null)
+            {
+                //writer.WriteEndElement();
+            }
+
+            bool hasElements = false;
+
+            var xmlElements = (from a in elementFo._attributes where a.Value is FluentObject select a);
+            foreach (var xmlElement in xmlElements)
+            {
+                // TODO: fix
+                hasElements = true;
+                WriteXmlElement(writer, xmlElement.Value, xmlElement.Key);
+            }
+            // TODO: selectmany
+            xmlElements = (from a in elementFo._dictionaryElements where a.Value is FluentObject && a.Key is int select new KeyValuePair<string, dynamic>(name ?? "unnamed", a.Value));
+            foreach (var xmlElement in xmlElements)
+            {
+                // TODO: if dictionary["abcd"], use key and value? (some dictionary keys may not be valid xml
+                hasElements = true; // TODO: fix
+                WriteXmlElement(writer, xmlElement.Value, xmlElement.Key);
+            }
+            // TODO: selectmany
+            xmlElements = (from a in elementFo._dictionaryElements where a.Value is FluentObject && !(a.Key is int) select new KeyValuePair<string, dynamic>(a.Key.ToString(), a.Value));
+            foreach (var xmlElement in xmlElements)
+            {
+                // TODO: if dictionary["abcd"], use key and value? (some dictionary keys may not be valid xml
+                hasElements = true; // TODO: fix
+                WriteXmlElement(writer, xmlElement.Value, xmlElement.Key);
+            }
+
+            if (name != null)
+            {
+                if (hasElements)
+                {
+                    writer.WriteFullEndElement();
+                }
+                else
+                {
+                    writer.WriteEndElement();
+                }
+            }
+        }
+
+        private static void WriteXmlAttributes(XmlWriter writer, IEnumerable<KeyValuePair<string, string>> attributes)
+        {
+            foreach (var attribute in attributes)
+            {
+                writer.WriteAttributeString(attribute.Key, attribute.Value);
+            }
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            bool first = true;
+            //reader.ReadStartElement();
+
+            while (reader.NodeType != XmlNodeType.EndElement)
+            {
+                // first name will always be FluentObject if WriteXml was used
+                if (first && reader.Name == "FluentObject")
+                {
+                    first = false;
+                }
+                else
+                {
+                    // TODO: check for existence, convert to array
+                    _dictionaryElements.Add(reader.Name, new FluentObject());
+                }
+
+                if (reader.HasAttributes)
+                {
+                    while (reader.MoveToNextAttribute())
+                    {
+                        _attributes.Add(reader.Name, reader.Value);
+                    }
+                }
+
+                reader.MoveToContent();
+                //_dictionaryElements.Add(entity.Key, entity);
+
+                if (!reader.Read())
+                {
+                    break;
+                }
+            }
+
+            //reader.ReadEndElement();
+        }
+    }
+}
---
+++ FluentObjectsTests/TestFluentObjectXmlSerializer.cs
@@ -0,0 +1,136 @@
+﻿using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+using FluentObjects;
+using NUnit.Framework;
+
+namespace FluentObjectsTests
+{
+    [TestFixture]
+    public class TestFluentObjectXmlSerializer
+    {
+        [Test]
+        public void TestBasicXml()
+        {
+            dynamic o = new FluentObject();
+
+            o.test = "asdf";
+            
+            XmlSerializer x = new XmlSerializer(typeof(FluentObject));
+
+            string output;
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                x.Serialize(stream, o);
+                output = GetUtf8FromMemoryStream(stream);
+            }
+
+            Assert.AreEqual("<?xml version=\"1.0\"?>\r\n<FluentObject test=\"asdf\" />", output);
+        }
+
+        [Test]
+        public void TestXmlObjects()
+        {
+            dynamic o = new FluentObject();
+
+            o.test.on.you = "true";
+
+            XmlSerializer x = new XmlSerializer(typeof(FluentObject));
+
+            string output;
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                x.Serialize(stream, o);
+                output = GetUtf8FromMemoryStream(stream);
+            }
+
+            Assert.AreEqual("<?xml version=\"1.0\"?>\r\n<FluentObject>\r\n  <test>\r\n    <on you=\"true\" />\r\n  </test>\r\n</FluentObject>", output);
+        }
+
+        [Test]
+        public void TestXmlArrays()
+        {
+            dynamic o = new FluentObject();
+
+            o.test[0].element = "true";
+            o.test[1].element = "false";
+
+            XmlSerializer x = new XmlSerializer(typeof(FluentObject));
+
+            string output;
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                x.Serialize(stream, o);
+                output = GetUtf8FromMemoryStream(stream);
+            }
+
+            Assert.AreEqual("<?xml version=\"1.0\"?>\r\n<FluentObject>\r\n  <test>\r\n    <test element=\"true\" />\r\n    <test element=\"false\" />\r\n  </test>\r\n</FluentObject>", output);
+        }
+
+        [Test]
+        public void TestXmlDictionary()
+        {
+            dynamic o = new FluentObject();
+
+            o.test["abc"].element = "true";
+            o.test["def"].element = "false";
+
+            XmlSerializer x = new XmlSerializer(typeof(FluentObject));
+
+            string output;
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                x.Serialize(stream, o);
+                output = GetUtf8FromMemoryStream(stream);
+            }
+
+            // TODO: fix because dictionary keys may not be valid xml element names and because this will not deserialize to a dictionary
+            Assert.AreEqual("<?xml version=\"1.0\"?>\r\n<FluentObject>\r\n  <test>\r\n    <abc element=\"true\" />\r\n    <def element=\"false\" />\r\n  </test>\r\n</FluentObject>", output);
+        }
+
+        // TODO: more tests to assert dictionary/array re-serialize back to dictionary/array (I assume they will not)
+
+        [Test]
+        public void TestReadXmlString()
+        {
+            XmlSerializer x = new XmlSerializer(typeof(FluentObject));
+            dynamic o;
+
+            const string original = "<?xml version=\"1.0\"?>\r\n<FluentObject test=\"asdf\" />";
+            byte[] buffer = Encoding.UTF8.GetBytes(original);
+            using (Stream stream = new MemoryStream(buffer))
+            {
+                o = x.Deserialize(stream);
+            }
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                FluentObject fo = (FluentObject)o;
+                x.Serialize(stream, fo);
+                string reserialized = GetUtf8FromMemoryStream(stream);
+                Assert.AreEqual(original, reserialized);
+            }
+        }
+
+        [Test]
+        public void TestToXmlString()
+        {
+            dynamic o = new FluentObject();
+
+            o.test = "asdf";
+
+            string output = ((FluentObject)o).ToXmlString("CustomName");
+            
+            Assert.AreEqual("<?xml version=\"1.0\" encoding=\"utf-16\"?><CustomName test=\"asdf\" />", output);
+        }
+
+        private string GetUtf8FromMemoryStream(MemoryStream stream)
+        {
+            return Encoding.UTF8.GetString(stream.GetBuffer(), 0, (int)stream.Position);
+        }
+    }
+}
